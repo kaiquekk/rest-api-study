a64b46ea00acddbda2871cb202c596a5f405ffc0
Kaique Kazuyoshi Komata <kaique.komata@opus-software.com.br>
2020-01-23 11:03:00 -0300

    Add eslint and babel modules
    Signed-off-by: Kaique Kazuyoshi Komata <kaique.komata@opus-software.com.br>

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,7 @@
 const app = require('../src/app');
 const port = process.env.PORT || 4545;
 
+/* eslint-disable no-console */
 app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
 });
